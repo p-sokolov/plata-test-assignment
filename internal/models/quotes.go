c814d8a69c8ccb9e11ff2cddddf5c93cf9eefce9
hryak228pizza <I.Pavel-2004@mail.ru>
2026-09-14 04:22:00 +0300

    refactor(models): refactor domain models for convinient data model usage

--- internal/models/quotes.go
+++ internal/models/quotes.go
@@ -1,31 +1,28 @@
 package models
 
 import (
-	"github.com/google/uuid"
 	"time"
+
+	"github.com/google/uuid"
 )
 
-type Quotes struct {
+type QuoteUpdate struct {
 	ID            uuid.UUID
 	CurrencyPair  string
 	Status        string
-	Rate          float32
-	ErrorMessage  string
+	Rate          *float64
+	ErrorMessage  *string
 	AttemptCount  int32
 	NextAttemptAt time.Time
 	LockedUntil   *time.Time
-	LeaseToken    uuid.UUID
+	LeaseToken    *uuid.UUID
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 }
 
-type IdempotencyKey struct {
-	Key          string
-	RequestHash  string
-	UpdateID     uuid.UUID
-	ResponseCode int16
-	ExpiresAt    time.Time
-	CreatedAt    time.Time
+type RefreshInput struct {
+	CurrencyPair   string
+	IdempotencyKey string
 }
 
 type OutboxEvent struct {
