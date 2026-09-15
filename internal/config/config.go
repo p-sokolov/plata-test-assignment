467caa765bff6c6cacb398a9b0368080fc9d1f83
hryak228pizza <I.Pavel-2004@mail.ru>
2026-09-15 08:12:46 +0300

    refactor(config.go): add backoffs parameters

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,8 +10,8 @@ import (
 type Config struct {
 	HttpSrv  httpServer
 	Postgres postgres
-	FX 		 fxConfig
-	Worker	 workerConfig
+	FX       fxConfig
+	Worker   workerConfig
 }
 
 type httpServer struct {
@@ -24,15 +24,17 @@ type postgres struct {
 }
 
 type fxConfig struct {
-	BaseURL string `env:"FX_BASE_URL" env-required:"true"`
-	APIKey string `env:"FX_ACCESS_KEY" env-required:"true"`
+	BaseURL string        `env:"FX_BASE_URL" env-required:"true"`
+	APIKey  string        `env:"FX_ACCESS_KEY" env-required:"true"`
 	Timeout time.Duration `env:"FX_TIMEOUT" env-default:"10s"`
 }
 
 type workerConfig struct {
-	Interval time.Duration `env:"WORKER_POLL_INTERVAL" env-default:"1s"`
-	Duration time.Duration `env:"WORKER_LEASE_DURATION" env-default:"30s"`
-	Attempts int32 `env:"WORKER_MAX_ATTEMPTS" env-default:"3"`
+	Interval         time.Duration `env:"WORKER_POLL_INTERVAL" env-default:"1s"`
+	Duration         time.Duration `env:"WORKER_LEASE_DURATION" env-default:"30s"`
+	Attempts         int32         `env:"WORKER_MAX_ATTEMPTS" env-default:"3"`
+	BackoffBaseDelay time.Duration `env:"WORKER_BASE_DELAY" env-default:"1s"`
+	BackoffMaxDelay  time.Duration `env:"WORKER_MAX_DELAY" env-default:"50s"`
 }
 
 // New creates a new config instance
